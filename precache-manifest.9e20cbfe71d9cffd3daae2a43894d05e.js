806e2e12abc3d014bc99be66d47e309cb9a3a55f
quickmaffs-dev <quickmaffs7@gmail.com>
2020-07-25 12:28:20 +1000

    Updates

---
+++ precache-manifest.9e20cbfe71d9cffd3daae2a43894d05e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8950369e8d80856d5268114aad484787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e0ea5ea2057f4c740c",
+    "url": "/static/css/main.a7cf942f.chunk.css"
+  },
+  {
+    "revision": "c9a46a1e1e61e58068a8",
+    "url": "/static/js/2.bf456495.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.bf456495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e0ea5ea2057f4c740c",
+    "url": "/static/js/main.c5362695.chunk.js"
+  },
+  {
+    "revision": "b9329d5bcdc2eb5f1da0",
+    "url": "/static/js/runtime-main.7c196ed6.js"
+  },
+  {
+    "revision": "a33a624f6a2b4c2baa60dc5312d570dc",
+    "url": "/static/media/icon.a33a624f.png"
+  },
+  {
+    "revision": "46a9d6a54ae717f711025a40917177b3",
+    "url": "/static/media/trigDepression.46a9d6a5.png"
+  },
+  {
+    "revision": "d17fcac71b709294943d728592621aaa",
+    "url": "/static/media/trigElevation.d17fcac7.png"
+  }
+]);
